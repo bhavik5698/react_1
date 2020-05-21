ccc7f97b3a1fc048f069f55e60ec984b1e75813f
bhavik5698 <bhavik5698@gmail.com>
2020-05-21 17:07:57 +0530

    react_app 1

---
+++ src/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import
+import App from './App';
+
+
+const message=(<div><h1>hello world</h1></div>)
+ReactDOM.render(
+  message,document.getElementById('root')
+)
--- src/index.js
+++ src/index.js
@@ -4,14 +4,117 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let data={
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+class Mydata extends React.Component {
+  constructor(props) {
+  super(props);
+  this.state = { username: '' ,
+eng : 0, maths : 0 ,ss : 0 };
+    }
+
+    nameChange =(e)=>{
+      this.setState({username: e.target.value});
+    this.setState({ name : e.target.value })
+}
+    carName =(e)=> {
+   this.setState({ car : e.target.value})
+}
+
+eng =(e)=> {
+this.setState({ eng : e.target.value})
+}
+maths =(e)=> {
+this.setState({ maths : e.target.value})
+}
+ss =(e)=> {
+this.setState({ ss : e.target.value})
+}
+
+ total=(eng,maths,ss)=> {
+      var total=eng + maths + ss
+
+    return( console.log("total  "+total ))
+            }
+
+  change =(e)=>{
+      document.body.style.backgroundColor = e.target.value
+
+    }
+
+
+
+  load = () =>(
+
+    console.log ("name: " + this.state.name),
+    console.log ("car " + this.state.car),
+    console.log ("english " + this.state.eng),
+    console.log ("maths " + this.state.maths),
+    console.log ("social since " + this.state.ss),
+    this.total(this.state.eng,this.state.maths,this.state.ss)
+  )
+
+    render() {
+
+    return(
+      <section>
+        <div>
+        <form>
+          <h1>Hello {this.state.username}</h1>
+            <p>Enter your name:</p>
+            <input type="text" onChange={this.nameChange} />
+            <p>Enter your favorite color</p>
+            <input type="color" id="bgcolor"  onChange={this.change}/>
+            <p>Enter your favorite car</p>
+            <input type="text" onChange={this.carName}/>
+            <p>Enter your english marks:</p>
+            <input type="number" onChange={this.eng} />
+            <p>Enter your maths marks:</p>
+            <input type="number" onChange={this.maths} />
+            <p>Enter your social since marks:</p>
+            <input type="number" onChange={this.ss} />
+          <p>  <button type="button" onClick={this.load}>Submit </button></p>
+    </form>
+        </div>
+
+      </section>
+    )
+
+  }
+
+}
+
+class Car extends React.Component {
+  render() {
+    const{brand,model,color,year}=this.props
+    return (
+      <div>
+        <h1>My {brand}</h1>
+        <p>
+          It is a {color} " " {model} from {year}.
+        </p>
+      </div>
+    );
+
+  }
+}
+
+ReactDOM.render(
+  <Mydata />,document.getElementById('root')
+)
+ReactDOM.render(
+  <Car
+  brand={data.brand}
+  color={data.color}
+  model={data.model}
+  year={data.year}
+/>,
+  document.getElementById('car')
+)
 serviceWorker.unregister();
